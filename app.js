47ed1dd9b39c5b50775257bea996bff2a0cdc976
tacimavi1516 <161332811+tacimavi1516@users.noreply.github.com>
2024-11-22 08:46:15 -0300

    hifry

--- app.js
+++ app.js
@@ -2,24 +2,27 @@ function criaCartao(categoria, pergunta, resposta) {
     let container = document.getElementById('container')
     let cartao = document.createElement('article')
     cartao.className = 'cartao'
-}
-cartao.innerHTML = `
-<div class="cartao__conteudo">
-<h3>Programação</h3>
-<div class="cartao__conteudo__pergunta">
-        <p>O que é JavaScript?</p>
-</div>
-<div class="cartao__conteudo__resposta">
-        <p>O JavaScript é uma linguagem de programação</p>
-</div>
-</div>
-`
-container.appendChild(cartao) 
 
-let respostaEstaVisivel = false
+    cartao.innerHTML = `
+    <div class="cartao__conteudo">
+    <h3>${categoria}</h3>
+    <div class="cartao__conteudo__pergunta">
+        <p>${pergunta}</p>
+    </div>
+    <div class="cartao__conteudo__resposta">
+        <p>${resposta}</p>
+    </div>
+    </div>
+    `
 
-function viraCartao() {
-    respostaEstaVisivel = !respostaEstaVisivel
-    cartao.classList.toggle('active', respostaEstaVisivel)
+    let respostaEstaVisivel = false
+
+    function viraCartao() {
+        respostaEstaVisivel = !respostaEstaVisivel
+        cartao.classList.toggle('active', respostaEstaVisivel)
+    }
+    cartao.addEventListener('click', viraCartao)
+
+
+    container.appendChild(cartao)
 }
-cartao.addEventListener('click', viraCartao)
